9e24093e11dab7e93a2e2befcbd264f31d579570
Shreyachhotray <139445739+Shreyachhotray@users.noreply.github.com>
2023-08-07 01:02:52 +0530

    Add files via upload

---
+++ thermo.js
@@ -0,0 +1,15 @@
+function convertTemperature() {
+    const celsiusInput = document.getElementById("celsius");
+    const fahrenheitInput = document.getElementById("fahrenheit");
+    if (celsiusInput.value !== "") {
+        const celsiusValue = parseFloat(celsiusInput.value);
+        const fahrenheitValue = (celsiusValue * 9/5) + 32;
+        fahrenheitInput.value = fahrenheitValue.toFixed(2);
+    }
+    if (fahrenheitInput.value !== "") {
+        const fahrenheitValue = parseFloat(fahrenheitInput.value);
+        const celsiusValue = (fahrenheitValue - 32) * 5/9;
+        celsiusInput.value = celsiusValue.toFixed(2);
+    }
+}
+convertTemperature();
